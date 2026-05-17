de4df35cab8c2b29db352fbb053bbbf0a04548d1
Russell Johnson <russjohnson09@gmail.com>
2026-05-17 16:53:50 -0400

    updates

--- godot/punch_2.gd
+++ godot/punch_2.gd
@@ -6,5 +6,10 @@ extends Node2D
 func _ready():
 	print("start")
 	
-	$AnimationPlayer.play("new_animation")
+	$AnimationPlayer.play("new_animation", -1, 1.2)
 	pass
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	
+	pass # Replace with function body.
