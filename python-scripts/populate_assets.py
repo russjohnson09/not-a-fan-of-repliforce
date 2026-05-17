47e926a994e7edcb860b739135323c168a8d3ccb
Russell Johnson <russjohnson09@gmail.com>
2026-05-17 13:54:18 -0400

    updates

--- python-scripts/populate_assets.py
+++ python-scripts/populate_assets.py
@@ -1,6 +1,9 @@
 
 from yt_dlp import YoutubeDL
 import os
+from moviepy import VideoFileClip, TextClip, CompositeVideoClip
+import pandas as pd
+import pathlib
 
 
 IRIS_DEATH = "https://www.youtube.com/watch?v=3IPIPLM8ELY"
@@ -16,19 +19,81 @@ def _get_full_path(downloaded, format = 'mp4', directory = _dir):
     filename = f'{title} [{id}].{format}'
     return  os.path.abspath(os.path.join(_dir, filename))
 
+
+def _parse_iris_audio():
+
+    pass
+
+
+def _get_timing(file_path):
+    df = pd.read_csv(file_path, keep_default_na=False)
+
+    dict_output = df.to_dict(orient="index")
+
+    print(dict_output)
+    # tests\test_font.py {0: {'start': 1000, 'text': 'PRESS'}, 1: {'start': 2000, 'text': ''}, 2: {'start': 3000, 'text': 'ZIP'}}
+    
+    result = []
+    for key in list(dict_output.keys()):
+        row = dict_output[key]
+        result.append(row)
+
+    print(result)
+    return result
+
+
+def _make_path_directory(filepath):
+    dir_path = os.path.dirname(filepath)
+    print(f'make directory if not exists {dir_path}')
+    pathlib.Path(dir_path).mkdir(parents=True, exist_ok=True) 
+
+
+def _get_mp3_filename(assets_dir, text: str):
+    filename = text
+    filename = f'{filename}.mp3'
+    return os.path.abspath(os.path.join(assets_dir, filename)) 
+
+def _save_mp3_from_timing_array(assets_dir, video: VideoFileClip, timing_array):
+
+    count = 0
+    for timing in timing_array:
+        count += 1
+        print(timing)
+
+        filepath = _get_mp3_filename(assets_dir, f'{count}. {timing.get('text')}')
+
+        clipped_video = video.subclipped(timing.get('start'), timing.get('end'))
+
+        _make_path_directory(filepath)
+        clipped_video.audio.write_audiofile(filepath)
+
+def _audio_clips_iris_death(original_video):
+    timing_array = _get_timing(os.path.dirname(__file__) + '/iris_death.csv')
+    print(timing_array)
+    assets_dir = os.path.dirname(__file__) + '/assets/'
+    _save_mp3_from_timing_array(assets_dir, original_video, timing_array)
+    pass
+
+
 def _download_iris():
     # os.chdir(_assets_dir, 'iris')
     # https://stackoverflow.com/questions/41240726/change-the-output-name-when-download-with-youtube-dl-using-python
     ydl_opts = {'outtmpl': 'assets/iris_death.mp4'}
     with YoutubeDL(ydl_opts) as ydl:
         downloaded = ydl.extract_info(IRIS_DEATH)
-        print("result")
-        print(downloaded.get('id'))
-        print(downloaded.get('title'))
-        fullpath = _get_full_path(downloaded)
-        print(fullpath)
+        # print("result")
+        # print(downloaded.get('id'))
+        # print(downloaded.get('title'))
+        # fullpath = _get_full_path(downloaded)
+        # print(fullpath)
         # os.rename(fullpath, os.path.abspath(os.path.join(_assets_dir, 'iris_death.mp4')))
         # Megaman X4： Iris's Death [3IPIPLM8ELY].mp4
+    
+    
+    original_video = VideoFileClip('assets/iris_death.mp4')
+    original_video.audio.write_audiofile('assets/iris_death.mp3')
+    _audio_clips_iris_death(original_video)
+
 
 
 def main():
